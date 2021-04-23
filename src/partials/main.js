7de812273ec58b808f8c0dc2eca2397b66ffb086
IiIymik <71165032+IiIymik@users.noreply.github.com>
2021-04-23 21:53:10 +0300

    done 1 card

--- src/partials/main.js
+++ src/partials/main.js
@@ -3,3 +3,8 @@ console.log(cards);
 import cardInfo from '../menu.json';
 console.log(cardInfo);
 console.log(cards(cardInfo[0]));
+
+const menuEl = document.querySelector(".js-menu");
+
+
+menuEl.innerHTML = cards(cardInfo[0])
