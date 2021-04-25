ef5a7ddf94c0b68680e526577d67ea9da2509e10
IiIymik <71165032+IiIymik@users.noreply.github.com>
2021-04-25 21:28:37 +0300

    done h.w 10

--- src/partials/changeThemeWidget.js
+++ src/partials/changeThemeWidget.js
@@ -1,11 +1,52 @@
 import { Theme }     from './constants.js';
-// console.log(Theme);
+
 const bodyEl = document.querySelector('body');
 
 const checkBoxEl = document.getElementById('theme-switch-toggle');
 
 const checkLocalSt = localStorage.getItem('Theme');
 
-  
+let currentValueTheme = '';
 
+checkBoxEl.addEventListener('change', checkBox);
+
+checkValueLS();
+
+function checkBox(e) {
+    if (currentValueTheme === Theme.DARK) {
+        addDarkTheme();
+    }
+    return checkBoxEl.checked ? addDarkTheme() : addLightTheme();
+}
+
+ function addLightTheme() {
+    bodyEl.classList.remove(Theme.DARK);
+    bodyEl.classList.add(Theme.LIGHT);
+    localStorage.setItem('Theme', Theme.LIGHT);
+     checkBoxEl.removeAttribute('checked');
+     currentValueTheme = localStorage.getItem('Theme')
+};
   
+  function addDarkTheme() {
+    bodyEl.classList.remove(Theme.LIGHT);
+    bodyEl.classList.add(Theme.DARK);
+      localStorage.setItem('Theme', Theme.DARK);
+      currentValueTheme = localStorage.getItem('Theme')
+    console.log(currentValueTheme);
+
+};
+
+function checkValueLS() {
+    if (checkLocalSt !== null) {
+        if (checkLocalSt === Theme.DARK) {
+            checkBoxEl.checked = true;
+            console.log("1", checkBoxEl.checked)
+            checkBoxEl.removeAttribute('checked');
+            addDarkTheme();
+        } else {
+            console.log("2")
+
+            addLightTheme();
+        }
+    }
+}
--- src/partials/main.js
+++ src/partials/main.js
@@ -3,39 +3,12 @@ import cards from '../templates/card.hbs';
 import cardInfo from '../menu.json';
 
 import { Theme } from './constants.js';
-console.log(Theme); 
+
 
 window.onload = () => {
     const menuEl = document.querySelector(".js-menu");
-    const defaultTheme = localStorage.setItem('Theme', Theme.LIGHT);
     const markup = cards(cardInfo);
     menuEl.insertAdjacentHTML('beforeend', markup);
  }
 
-// bodyEl.classList.add(Theme.LIGHT);
-// window.onload = () => {
-// function setTh() {
-//   if (checkLocalSt !== null) {
-//     return checkLocalSt === Theme.LIGHT ? localStorage.setItem('Theme', Theme.LIGHT) : localStorage.setItem('Theme', Theme.DARK);
-//   }
-// };
-
-
-//  function checkBoxValue(e) {
-//     return checkBoxEl.checked ? addDarkTheme() : addLightTheme();
-//   }
-
-
-  // function addLightTheme() {
-  //   bodyEl.classList.remove(Theme.DARK);
-  //   bodyEl.classList.add(Theme.LIGHT);
-  //   localStorage.setItem('Theme', Theme.LIGHT);
-  // };
-
-  // function addDarkTheme() {
-  //   bodyEl.classList.remove(Theme.LIGHT);
-  //   bodyEl.classList.add(Theme.DARK);
-  //   localStorage.setItem('Theme', Theme.DARK);
-  // };
-// }
 
